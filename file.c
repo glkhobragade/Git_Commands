fb4b11d247c252d9c6cdbab6b3b74149f8ab7824
Ghansham <glkhobragade@gmail.com>
2017-05-12 05:30:13 +0530

    --> changes for seeing the prog entry for file descriptor when file is opened.

---
+++ file.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include<fcntl.h>
+int main()
+{
+	FILE *fp;
+	fp = fopen("a.txt", "a+");
+	if(!fp)
+	printf("Opened file successfully");
+	sleep(100);
+	fclose(fp);
+	return 0;
+}
