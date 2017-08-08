1990fe567c8f0a0024dc95da0df63d0f6e9ee4aa
Ghansham <glkhobragade@gmail.com>
2017-08-08 09:02:46 +0530

    auto_pointer example and its properties

---
+++ smartp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+
+/* auto_ptr transfer the ownership of the pointer, so another pointer can remove the memory block assigned,
+ * thus leaving the earlier pointer becoming dangled, thats why this program is getting crashed.
+ * more information on 
+ * https://www.codeproject.com/Articles/541067/Cplusplus-Smart-Pointers  */
+
+
+using namespace std;
+//***************************************************************
+class Test
+{
+    public:
+    Test(int a = 0 ) : m_a(a)
+    {
+    }
+    ~Test( )
+    {
+        cout<<"Calling destructor"<<endl;
+    }
+    public:
+        int m_a;
+};
+
+
+//***************************************************************
+void Fun(auto_ptr<Test> p1 )
+{
+    cout<<p1->m_a<<endl;
+}
+//***************************************************************
+int main( )
+{
+    std::auto_ptr<Test> p( new Test(5) ); 
+    Fun(p);
+    cout<<p->m_a<<endl;
+    return 0;
+} 
