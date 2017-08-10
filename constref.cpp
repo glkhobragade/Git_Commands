7d39b07ee282c70951be163c82386366ecd1e190
Ghansham <glkhobragade@gmail.com>
2017-08-10 10:49:32 +0530

    minor changes

--- constref.cpp
+++ constref.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void foo(int& i)
 {
     cout << "\nInside foo()";
+    i++;
     cout << i;
 }
 
@@ -18,7 +19,8 @@ int main()
     std::cout << "\nHello World\n";
     int i=1;
     foo(i);
-    //foo(1); This will give an compilation error.
+    cout << "i value:" << i ;
+    //foo(1); //This will give an compilation error, as it generate the const int temp object for which such function is not present;
     bar(1);
     return 0;
 }
