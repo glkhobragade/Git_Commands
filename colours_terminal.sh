a12a8d422764fc49df5dc140b4c71626f924f166
Ghansham <glkhobragade@gmail.com>
2017-09-07 09:29:20 +0530

    color terminal fonts

---
+++ colours_terminal.sh
@@ -0,0 +1,7 @@
+for code in {30..37}; do \
+echo -en "\e[${code}m"'\\e['"$code"'m'"\e[0m"; \
+echo -en "  \e[$code;1m"'\\e['"$code"';1m'"\e[0m"; \
+echo -en "  \e[$code;3m"'\\e['"$code"';3m'"\e[0m"; \
+echo -en "  \e[$code;4m"'\\e['"$code"';4m'"\e[0m"; \
+echo -e "  \e[$((code+60))m"'\\e['"$((code+60))"'m'"\e[0m"; \
+done
