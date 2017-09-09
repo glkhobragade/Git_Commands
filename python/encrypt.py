371e8550aa59e2b45c2ecca954fff1d6969d40e8
Ghansham <glkhobragade@gmail.com>
2017-09-09 21:15:29 +0530

    task has been put in seperate functions

--- python/encrypt.py
+++ python/encrypt.py
@@ -3,57 +3,81 @@ import sys
 from Crypto import Random
 import base64
 
-IV =  "\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF\x00"
 KEY = "\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B\x6B"
 
-print (AES.key_size[0])
-print (AES.block_size)
+def AES_padding(p, v, n):
+    obj = AES.new(KEY, AES.MODE_CBC, v)
 
-obj = AES.new(KEY, AES.MODE_CBC, IV)
-#message = "The answer is no where pres"
+    print("##The input string is: ", p)
+    ptext_len = len(p)
+    print ('message length= ',ptext_len)
+    pad_lenth = (16 - (ptext_len%16))
+    print ("Bytes of padding needs to be added is :", pad_lenth)
+
+
+    ##  ============== padding done ==================
+    padding = ""
+    for i in range(pad_lenth):
+        #padding += ("\\0x"+str(pad_lenth))
+        padding += (chr(pad_lenth))
+
+
+    print("pad length= ", len(padding))
+    print("## padding string is =", padding)
+
+
+    p += padding
+
+    ##  ============== Encryption done ==================
+    global ciphertext
+    ciphertext = obj.encrypt(p)
+    print ("##ciphertext C is =", ciphertext)
+
+    no_of_blocks= len(ciphertext)/16
+    print ("ciphertext blocks  =", len(ciphertext)/16)
+
+
+
+def AES_valid_padding(c, v, b):
+    isValid = False
+    print ("##ciphertext C is =", c)
+    ##  ============== Decrpytion done ==================
+
+    obj2 = AES.new(KEY, AES.MODE_CBC, v)
+    plain = obj2.decrypt(c)
+    print ("decrypted plain text is:", plain)
+
+
+    ##  ============== Padding Removed ==================
+    last = plain[len(plain)-1]
+    #p = (len(plain)-int(last, 16));
+    p= int(last.encode('hex'), 16)
+    print ("p=", p)
+    print ("last byte of paintext = ", last)
+    print ("text after padding removed", plain[:len(plain)-p])
+
+    for i in range(p):
+        x = plain[(16*b)-i-1]
+        if int(x.encode('hex'), 16) != p:
+            return False
+    return True
+
+
+
+
+ciphertext = ""
+IV =  "\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF\x00"
 message = raw_input('Please Enter the plain text here:')
-print ("The msg is: ", message)
 msg_len = len(message)
-
-print ('message length= ', msg_len)
-pad_lenth = (16 - (msg_len%16))
-print ("Bytes of padding needs to be added is :", pad_lenth)
-
-##  ============== padding done ==================
-padding = ""
-for i in range(pad_lenth):
-    #padding += ("\\0x"+str(pad_lenth))
-    padding += (chr(pad_lenth))
+AES_padding(message, IV, msg_len)
+result = AES_valid_padding(ciphertext, IV, len(ciphertext)/16)
+if result:
+    print("It is valid PKCS padding")
+else:
+    print("It is not a valid PKCS padding")
 
 
-print("pad length= ", len(padding))
-print("padding is =", padding)
-message += padding
-
-##  ============== Encryption done ==================
-
-ciphertext = obj.encrypt(message)
-#sys.stdout.write(ciphertext)
-print ("ciphertext is =", ciphertext)
-no_of_blocks= len(ciphertext)/16
-print ("ciphertext blocks  =", len(ciphertext)/16)
-
-
-##  ============== Decrpytion done ==================
-
-obj2 = AES.new(KEY, AES.MODE_CBC, IV)
-plain = obj2.decrypt(ciphertext)
-print ("\n")
-print ("plain text is:", plain)
-
-
-##  ============== Padding Removed ==================
-last = plain[len(plain)-1]
-#p = (len(plain)-int(last, 16));
-p= int(last.encode('hex'), 16)
-print ("last byte of paintext = ", last)
-print ("text after padding removed", plain[:len(plain)-p])
-
+'''
 print ("ciphertext length=", len(ciphertext))
 
 list_I = ['\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00','\0x00']
@@ -115,3 +139,4 @@ for block in range(no_of_blocks):
 
 
 print ("done and broken plain text is ", comp_plain)
+'''
