595d02e2a48223c2e53fe1702dc8c3d5993d5181
Ghansham <glkhobragade@gmail.com>
2017-09-10 11:21:37 +0530

    comment removed from encrypt.py

--- colours_terminal.sh
+++ colours_terminal.sh
@@ -1,7 +1,13 @@
-for code in {30..37}; do \
+#!/bin/bash
+colors=$(seq 1 3 50)
+for code in $colors ; do \
 echo -en "\e[${code}m"'\\e['"$code"'m'"\e[0m"; \
 echo -en "  \e[$code;1m"'\\e['"$code"';1m'"\e[0m"; \
 echo -en "  \e[$code;3m"'\\e['"$code"';3m'"\e[0m"; \
 echo -en "  \e[$code;4m"'\\e['"$code"';4m'"\e[0m"; \
 echo -e "  \e[$((code+60))m"'\\e['"$((code+60))"'m'"\e[0m"; \
 done
+
+foo="Hello"
+foo=$foo"World"
+echo $foo
